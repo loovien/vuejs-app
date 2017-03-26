a795153ad832dfa25c10b83059eb1df4ea025f71
rueen <rueenli@gmail.com>
2017-03-26 11:05:26 +0800

    忘记密码

--- src/components/user/resetpwd.vue
+++ src/components/user/resetpwd.vue
@@ -1,12 +1,24 @@
-<style scope>
-    .login {
-        box-shadow: wheat 3px 0 0;
-    }
-</style>
 <template>
-    <div>
+    <div class="wrap">
+        <header class="header">
+            <a href="" class="iconfont icon-back header-goback" @click="window.history.go(-1)"></a>
+            <h1 class="header-title">忘记密码</h1>
+        </header>
         <div class="login">
-            reset password
+            <form action="" class="form">
+                <div class="input-box">
+                    <span class="iconfont icon-iphone input-icon"></span>
+                    <input type="text" placeholder="手机号" v-model="credentials.mobile" class="ui-input">
+                </div>
+                <div class="input-box">
+                    <span class="iconfont icon-Password input-icon"></span>
+                    <input type="password" placeholder="密码" v-model="credentials.password" class="ui-input">
+                </div>
+                <span v-show="error.msg">{{error.msg}}</span>
+            </form>
+            <div class="btn-box">
+                <button @click="nextStep()" class="btn">下一步</button>
+            </div>
         </div>
     </div>
 </template>
@@ -25,7 +37,7 @@
             }
         },
         methods: {
-            submit: () => {
+            nextStep: () => {
                 this.$http.post("/resetpwd", this.credentials, (resp) => {
                     console.log(resp);
                 }).error((error) => {
@@ -35,3 +47,9 @@
         }
     }
 </script>
+
+<style scope>
+    .login {
+        box-shadow: wheat 3px 0 0;
+    }
+</style>
