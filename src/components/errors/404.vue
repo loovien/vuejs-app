3f643593c5337750158125cf5e2c96f070bb204b
rueen <rueenli@gmail.com>
2017-04-14 22:24:22 +0800

    404

--- src/components/errors/404.vue
+++ src/components/errors/404.vue
@@ -1,9 +1,32 @@
 <template>
-    <h1 class="c404"> Opos!! the page is missing!!!!</h1>
+	<div class="inner">
+		<span class="iconfont icon-404"></span>
+		<p class="text">抱歉！您访问的页面不存在……</p>
+		<div>
+			<div class="btn">去首页</div>
+		</div>
+	</div>
 </template>
 
 <style scope>
-    .c404 {
-        box-shadow: 10px 0 0 red;
+	.inner{
+		position: absolute;
+		width: 100%;
+		text-align: center;
+		top: 50%; left: 0;
+		-webkit-transform: translateY(-50%);
+            	transform: translateY(-50%);
+	}
+	.inner .icon-404{
+		font-size: 80px;
+		line-height: 80px;
+	}
+    .text {
+        color: #ffb400;
+    }
+    .inner .btn{
+    	width: 100px;
+    	margin: 20px auto;
+    	font-weight: normal;
     }
 </style>
