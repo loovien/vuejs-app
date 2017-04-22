170a12baa0f84c109009151d89159f30ab2df604
rueen <rueenli@gmail.com>
2017-04-22 10:27:07 +0800

    样式调整

--- src/components/shared/Datepicker.vue
+++ src/components/shared/Datepicker.vue
@@ -396,226 +396,238 @@
     }
 </script>
 
-<style scoped lang='less'>
-    ul{
-        padding: 0;
-        margin: 0;
-        list-style: none;
-    }
-    .date-picker{
-        position: relative;
-        height: 32px;
-    }
-    .input-wrapper{
-        border: 1px solid #d7d7d7;
-        background: #fff;
-        color: #333;
-        border-radius: 2px;
-        vertical-align: middle;
-        display: flex;
-        justify-content: space-between;
-        flex-flow: row nowrap;
-        align-items: center;
-        padding: 6px 10px 6px 4px;
-        height: 32px;
-        box-sizing: border-box;
-    }
-    .input{
-        height: 100%;
-        width: 100%;
-        font-size: inherit;
-        padding-left: 4px;
-        box-sizing: border-box;
-        outline: none;
-    }
-    .cancel-btn{
-        height: 14px;
-        width: 14px;
-    }
-    .date-panel{
-        position: fixed;
-        z-index: 5000;
-        border: 1px solid #eee;
-        box-sizing: border-box;
-        width: 100%;
-        padding: 5px 10px 10px;
-        box-sizing: border-box;
-        background-color: #fff;
-        transform: translateY(4px);
-        bottom: 0;
-        color: #333;
-    }
-    .panel-header{
-        display: flex;
-        flex-flow: row nowrap;
-        width: 100%;
-    }
-    .arrow-left, .arrow-right{
-        flex: 1;
-        font-size: 20px;
-        line-height: 2;
-        background-color: #fff;
-        text-align: center;
-        cursor: pointer;
-    }
-    .year-range{
-        font-size: 20px;
-        line-height: 2;
-        flex: 3;
-        display: flex;
-        justify-content: center;
-    }
-    .year-month-box{
-        flex: 3;
-        display: flex;
-        flex-flow: row nowrap;
-        justify-content: space-around;
-    }
-    .type-year, .type-month, .date-list{
-        background-color: #fff;
-    }
-    .year-box, .month-box{
-        transition: all ease .1s;
-        font-family: Roboto, sans-serif;
-        flex: 1;
-        text-align: center;
-        font-size: 20px;
-        line-height: 2;
-        cursor: pointer;
-        background-color: #fff;
-    }
-    .year-list, .month-list{
-        display: flex;
-        flex-flow: row wrap;
-        justify-content: space-between;
-        li{
-            font-family: Roboto, sans-serif;
-            transition: all .45s cubic-bezier(0.23, 1, 0.32, 1) 0ms;
-            cursor: pointer;
-            text-align: center;
-            font-size: 20px;
-            width: 33%;
-            padding: 10px 0;
-            &:hover{
-                background-color: #6ac1c9;
-                color: #fff;
-            }
-            &.selected{
-                background-color: #0097a7;
-                color: #fff;
-            }
-            &.invalid{
-                cursor: not-allowed;
-                color: #ccc;
-            }
-        }
-    }
-    .date-list{
-        display: flex;
-        flex-flow: row wrap;
-        justify-content: space-between;
-        .valid:hover{
-            background-color: #eee;
-        }
-        li{
-            transition: all ease .1s;
-            cursor: pointer;
-            box-sizing: border-box;
-            border-bottom: 1px solid #fff;
-            position: relative;
-            margin: 2px;
-            &:not(.firstItem){
-                margin-left: 10px;
-            }
-            .message{
-                font-family: Roboto, sans-serif;
-                font-weight: 300;
-                font-size: 14px;
-                position: relative;
-                height: 30px;
-                &.selected{
-                    .bg{
-                        background-color: rgb(0, 151, 167);
-                    }
-                    span{
-                        color: #fff;
-                    }
-                }
-                &:not(.selected){
-                    .bg{
-                        transform: scale(0);
-                        opacity: 0;
-                    }
-                    &:hover{
-                        .bg{
-                            background-color: rgb(0, 151, 167);
-                            transform: scale(1);
-                            opacity: .6;
-                        }
-                        span{
-                            color: #fff;
-                        }
-                    }
-                }
-                .bg{
-                    height: 30px;
-                    width: 100%;
-                    transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;
-                    border-radius: 50%;
-                    position: absolute;
-                    z-index: 10;
-                    top: 0;
-                    left: 0;
-                }
-                span{
-                    position: absolute;
-                    z-index: 20;
-                    left: 50%;
-                    top: 50%;
-                    transform: translate3d(-50%, -50%, 0);
-                }
-            }
-            &.invalid{
-                cursor: not-allowed;
-                color: #ccc;
-            }
-        }
-        
-    }
-    .weeks{
-        display: flex;
-        flex-flow: row wrap;
-        justify-content: space-between;
-        li{
-            font-weight: 600;
-        }
-    }
-    .weeks, .date-list{
-        width: 100%;
-        text-align: center;
-        .preMonth, .nextMonth{
-            color: #ccc;
-        }
-        li{
-            font-family: Roboto;
-            width: 30px;
-            height: 30px;
-            text-align: center;
-            line-height: 30px;
-        }
-    }
-    .toggle-enter, .toggle-leave-active{
-        opacity: 0;
-        transform: translateY(-10px);
-    }
-    .toggle-enter-active, .toggle-leave-active{
-        transition: all ease .2s;
-    }
-    .fade-enter, .fade-leave-active{
-        opacity: 0;
-        transform: scale3d(0, 0, 0);
-    }
-    .fade-enter-active, .fade-leave-active{
-        transition: all ease .1s;
-    }
+<style scoped>
+ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+.date-picker {
+  position: relative;
+  height: 32px;
+}
+.input-wrapper {
+  border: 1px solid #d7d7d7;
+  background: #fff;
+  color: #333;
+  border-radius: 2px;
+  vertical-align: middle;
+  display: flex;
+  justify-content: space-between;
+  flex-flow: row nowrap;
+  align-items: center;
+  padding: 6px 10px 6px 4px;
+  height: 32px;
+  box-sizing: border-box;
+}
+.input {
+  height: 100%;
+  width: 100%;
+  font-size: inherit;
+  padding-left: 4px;
+  box-sizing: border-box;
+  outline: none;
+}
+.cancel-btn {
+  height: 14px;
+  width: 14px;
+}
+.date-panel {
+  position: fixed;
+  z-index: 5000;
+  border: 1px solid #eee;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 5px 10px 10px;
+  box-sizing: border-box;
+  background-color: #fff;
+  transform: translatey(4px);
+  bottom: 0;
+  color: #333;
+}
+.panel-header {
+  display: flex;
+  flex-flow: row nowrap;
+  width: 100%;
+}
+.arrow-left,
+.arrow-right {
+  flex: 1;
+  font-size: 20px;
+  line-height: 2;
+  background-color: #fff;
+  text-align: center;
+  cursor: pointer;
+}
+.year-range {
+  font-size: 20px;
+  line-height: 2;
+  flex: 3;
+  display: flex;
+  justify-content: center;
+}
+.year-month-box {
+  flex: 3;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+}
+.type-year,
+.type-month,
+.date-list {
+  background-color: #fff;
+}
+.year-box,
+.month-box {
+  transition: all ease .1s;
+  font-family: Roboto, sans-serif;
+  flex: 1;
+  text-align: center;
+  font-size: 20px;
+  line-height: 2;
+  cursor: pointer;
+  background-color: #fff;
+}
+.year-list,
+.month-list {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+}
+.year-list li,
+.month-list li {
+  font-family: Roboto, sans-serif;
+  transition: all 0.45s cubic-bezier(0.23, 1, 0.32, 1) 0ms;
+  cursor: pointer;
+  text-align: center;
+  font-size: 20px;
+  width: 33%;
+  padding: 10px 0;
+}
+.year-list li:hover,
+.month-list li:hover {
+  background-color: #6ac1c9;
+  color: #fff;
+}
+.year-list li.selected,
+.month-list li.selected {
+  background-color: #0097a7;
+  color: #fff;
+}
+.year-list li.invalid,
+.month-list li.invalid {
+  cursor: not-allowed;
+  color: #ccc;
+}
+.date-list {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+}
+.date-list .valid:hover {
+  background-color: #eee;
+}
+.date-list li {
+  transition: all ease .1s;
+  cursor: pointer;
+  box-sizing: border-box;
+  border-bottom: 1px solid #fff;
+  position: relative;
+  margin: 2px;
+}
+.date-list li:not(.firstItem) {
+  margin-left: 10px;
+}
+.date-list li .message {
+  font-family: Roboto, sans-serif;
+  font-weight: 300;
+  font-size: 14px;
+  position: relative;
+  height: 30px;
+}
+.date-list li .message.selected .bg {
+  background-color: #0097a7;
+}
+.date-list li .message.selected span {
+  color: #fff;
+}
+.date-list li .message:not(.selected) .bg {
+  transform: scale(0);
+  opacity: 0;
+}
+.date-list li .message:not(.selected):hover .bg {
+  background-color: #0097a7;
+  transform: scale(1);
+  opacity: .6;
+}
+.date-list li .message:not(.selected):hover span {
+  color: #fff;
+}
+.date-list li .message .bg {
+  height: 30px;
+  width: 100%;
+  transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;
+  border-radius: 50%;
+  position: absolute;
+  z-index: 10;
+  top: 0;
+  left: 0;
+}
+.date-list li .message span {
+  position: absolute;
+  z-index: 20;
+  left: 50%;
+  top: 50%;
+  transform: translate3d(-50%, -50%, 0);
+}
+.date-list li.invalid {
+  cursor: not-allowed;
+  color: #ccc;
+}
+.weeks {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+}
+.weeks li {
+  font-weight: 600;
+}
+.weeks,
+.date-list {
+  width: 100%;
+  text-align: center;
+}
+.weeks .preMonth,
+.date-list .preMonth,
+.weeks .nextMonth,
+.date-list .nextMonth {
+  color: #ccc;
+}
+.weeks li,
+.date-list li {
+  font-family: Roboto;
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  line-height: 30px;
+}
+.toggle-enter,
+.toggle-leave-active {
+  opacity: 0;
+  transform: translatey(-10px);
+}
+.toggle-enter-active,
+.toggle-leave-active {
+  transition: all ease .2s;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+  transform: scale3d(0, 0, 0);
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all ease .1s;
+}
+
 </style>
