2b43d0af194d73153034b41959b86a9da595d5db
luowen <bigpao.luo@gmail.com>
2017-04-23 19:53:14 +0800

    debug

--- src/components/act/template_1/shared.vue
+++ src/components/act/template_1/shared.vue
@@ -296,46 +296,11 @@
 
                 */
 
-                wx.onMenuShareAppMessage({
-                    title: '互联网之子11',
-                    desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
-                    link: 'http://movie.douban.com/subject/25785114/',
-                    imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
-                    trigger: function (res) {
-                        // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-                        alert('用户点击发送给朋友');
-                    },
-                    success: function (res) {
-                        alert('已分享');
-                    },
-                    cancel: function (res) {
-                        alert('已取消');
-                    },
-                    fail: function (res) {
-                        alert(JSON.stringify(res));
-                    }
-                });
-
-                wx.onMenuShareTimeline({
-                    title: '互联网之子22',
-                    link: 'http://movie.douban.com/subject/25785114/',
-                    imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
-                    trigger: function (res) {
-                        // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-                        alert('用户点击分享到朋友圈');
-                    },
-                    success: function (res) {
-                        alert('已分享');
-                    },
-                    cancel: function (res) {
-                        alert('已取消');
-                    },
-                    fail: function (res) {
-                        alert(JSON.stringify(res));
-                    }
-                });
+                wxSrv.initWxJsConfig(currentUrl, wxShareConfig);
 
 
+
+                /*
                 const wxShareConfig = {
                     title: "这是一个测试分享标题",
                     desc: "这是一个分想描述",
@@ -345,8 +310,8 @@
                         alert("分享成功");
                     }
                 };
+                */
 
-                wxSrv.initWxJsConfig(currentUrl, wxShareConfig);
                 const starttime = (new Date(act.act_start_time)).getTime();
                 const endtime = (new Date(act.act_end_time)).getTime();
                 const today = new Date().getTime();
