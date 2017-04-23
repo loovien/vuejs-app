1a329f65b49de24e5ce37b09b84d2484cf6941b4
luowen <bigpao.luo@gmail.com>
2017-04-23 19:57:26 +0800

    debug

--- src/service/wxSrv.js
+++ src/service/wxSrv.js
@@ -14,7 +14,7 @@ export default class  WxSrv extends BaseSrv  {
                 wx.ready(function(){
                     alert("wxregister ok")
 
-                    wx.onMenuShareAppMessage({
+                    const share = {
                         title: '互联网之子',
                         desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
                         link: 'http://movie.douban.com/subject/25785114/',
@@ -32,10 +32,13 @@ export default class  WxSrv extends BaseSrv  {
                         fail: function (res) {
                             alert(JSON.stringify(res));
                         }
-                    });
-/*                    console.log("share register success")
-                    wx.onMenuShareTimeline(shareConfig);
-                    wx.onMenuShareAppMessage(shareConfig);*/
+                    };
+                    wx.onMenuShareTimeline(share);
+                    wx.onMenuShareAppMessage(share);
+
+                    console.log("share register success")
+                    // wx.onMenuShareTimeline(shareConfig);
+                    // wx.onMenuShareAppMessage(shareConfig);
                 });
             } else {
                 console.log("微信分享搓搓", wxResp);
