3f4dcd64ebc62b2ab6daab72abd985d07a481857
luowen <bigpao.luo@gmail.com>
2017-04-23 19:50:36 +0800

    debug

--- src/components/act/template_1/shared.vue
+++ src/components/act/template_1/shared.vue
@@ -200,6 +200,7 @@
     import Modal from '../../shared/modal.vue'
     import Fixed from '../../shared/fixed.vue'
     import WxSrv from '../../../service/wxSrv';
+    import wx from 'wx';
 
     export default {
         data() {
@@ -292,7 +293,47 @@
                         alert("分享成功");
                     }
                 };
+
                 */
+
+                wx.onMenuShareAppMessage({
+                    title: '互联网之子11',
+                    desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
+                    link: 'http://movie.douban.com/subject/25785114/',
+                    imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
+                    trigger: function (res) {
+                        // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+                        alert('用户点击发送给朋友');
+                    },
+                    success: function (res) {
+                        alert('已分享');
+                    },
+                    cancel: function (res) {
+                        alert('已取消');
+                    },
+                    fail: function (res) {
+                        alert(JSON.stringify(res));
+                    }
+                });
+
+                wx.onMenuShareTimeline({
+                    title: '互联网之子22',
+                    link: 'http://movie.douban.com/subject/25785114/',
+                    imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
+                    trigger: function (res) {
+                        // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+                        alert('用户点击分享到朋友圈');
+                    },
+                    success: function (res) {
+                        alert('已分享');
+                    },
+                    cancel: function (res) {
+                        alert('已取消');
+                    },
+                    fail: function (res) {
+                        alert(JSON.stringify(res));
+                    }
+                });
 
 
                 const wxShareConfig = {
