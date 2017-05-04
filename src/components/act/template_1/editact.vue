6da7e85e6ace0d194e9b3e0f4331fa4772997c0d
luowen <bigpao.luo@gmail.com>
2017-05-04 22:30:18 +0800

    debug

--- src/components/act/template_1/editact.vue
+++ src/components/act/template_1/editact.vue
@@ -92,7 +92,7 @@
                         <img :src="item" alt="" class="img-w100">
                     </template>
                     <template v-show="act.images.length < 6">
-                        <input type="file" accept="image/jpeg,image/jpg,image/png" capture="camera" @change="onFileChange" placeholder="请输入奖品描述" class="upload-file">
+                        <input type="file" accept="image/jpeg,image/jpg,image/png;capture=camera" style="width:100%" @change="onFileChange" placeholder="请输入奖品描述" class="upload-file">
                         <span class="icon-upload iconfont"></span>
                         <p class="text-center">最多只能上传6张图片</p>
                     </template>
