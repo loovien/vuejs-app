81700d29c115833849e8326ac2eb6b9432448205
luowen <bigpao.luo@gmail.com>
2017-05-08 20:28:45 +0800

    debug

--- src/components/act/template_1/newact.vue
+++ src/components/act/template_1/newact.vue
@@ -94,7 +94,7 @@
                         <img :src="item" alt="" class="img-w100">
                     </template>
                     <template v-show="act.images.length < 6">
-                        <input type="file" capture="camera" @change="onFileChange" class="upload-file" accept="image/png, image/gif, image/jpeg" >
+                        <input type="file" @change="onFileChange" class="upload-file" accept="image/*" >
                         <br/> <br/>
                         <span class="icon-upload iconfont"></span>
                         <p class="text-center">最多只能上传6张图片</p>
